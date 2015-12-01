fbd4156549b357a904874c272ca9d48a82034001
Travis Haby <travishaby@gmail.com>
2015-12-01 13:14:58 -0700

    implemented creation of object when save button is pressed idea populates on the page without reloading, feature test is passing to confirm functionality

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,19 +1,30 @@
 function createIdea(){
 
   $("#save-button").on('click', function(){
-    var ideaTitle = $('#new-title').val()
-    var ideaBody = $('#new-body').val()
+    var ideaTitle = $('#new-title').val();
+    var ideaBody = $('#new-body').val();
 
-    $('#new-title').val('')
-    $('#new-body').val('')
-    
+    $('#new-title').val('');
+    $('#new-body').val('');
+
     var postParams = {
       idea: {
         title: ideaTitle,
          body: ideaBody
       }
-    }
-    console.log(postParams)
-  });
+    };
 
+    $.ajax({
+      type: 'POST',
+      url:  '/api/v1/ideas',
+      data: postParams,
+      success: function(newIdea) {
+        renderIdea(newIdea);
+      },
+      error: function(error) {
+        console.log(error.responseText)
+      }
+    });
+
+  });
 };
