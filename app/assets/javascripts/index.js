4db3fd978917acc9fcd5912bb9081730801ef888
Travis Haby <travishaby@gmail.com>
2015-12-01 14:08:19 -0700

    switched body length truncation from css to js solution, extracted this logic into a separate method in index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -17,8 +17,15 @@ function renderIdea(idea){
                           + idea.id + '"><h3>'
                           + idea.title + ' ('
                           + idea.quality + ')'
-                          + '</h3><h4 class="truncate">'
-                          + idea.body
+                          + '</h3><h4 class="">'
+                          + truncateBody(idea.body, 100)
                           + '</h4><button class="btn" id="delete-'
                           + idea.id + '">Delete</div>');
 };
+
+function truncateBody(body, maxLength){
+  var trimmedString = body.substr(0, maxLength);
+  return trimmedString = trimmedString.substr(0,
+                  Math.min(trimmedString.length,
+                  trimmedString.lastIndexOf(" "))) + '...'
+}
