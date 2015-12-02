6671bcd6c16bdcda08c962d46b7ec7059d92c1e4
Travis Haby <travishaby@gmail.com>
2015-12-01 17:07:11 -0700

    fixed bug and refactored implementation of truncating body

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -8,6 +8,7 @@ function allIdeas(){
       ideas.forEach(function(idea){
         renderIdea(idea);
       })
+      deleteIdea(); //this is here because it wasnt triggering in idea_box.js
     })
     .fail(function(){ console.log("api request failed") });
 };
@@ -17,15 +18,21 @@ function renderIdea(idea){
                           + idea.id + '"><h3>'
                           + idea.title + ' ('
                           + idea.quality + ')'
-                          + '</h3><h4 class="">'
+                          + '</h3><h5>'
                           + truncateBody(idea.body, 100)
-                          + '</h4><button class="btn" id="delete-'
-                          + idea.id + '">Delete</div>');
+                          + '</h5><button class="btn" id="delete-'
+                          + idea.id + '">Delete</button></div>');
 };
 
 function truncateBody(body, maxLength){
-  var trimmedString = body.substr(0, maxLength);
-  return trimmedString = trimmedString.substr(0,
-                  Math.min(trimmedString.length,
-                  trimmedString.lastIndexOf(" "))) + '...'
+  var trimmed = "";
+  var words = body.split(' ');
+  if (body.length < maxLength) { return body; };
+  var potential;
+  for (var i = 0; i < words.length; i++) {
+    potential = words[i] + trimmed;
+    if (potential.length > maxLength) { break; };
+    trimmed = ' ' + potential;
+  }
+  return trimmed.trim() + '...';
 }
