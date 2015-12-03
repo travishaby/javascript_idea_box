dfa9b6dcba2bfcc1246e25a3221092a11705f6af
Travis Haby <travishaby@gmail.com>
2015-12-02 18:18:43 -0700

    added in line to update quaility params before passing to update method

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -6,6 +6,7 @@ class Idea < ActiveRecord::Base
   def update_quality(quality_params)
     new_quality = Idea.qualities[self.quality] + quality_params[:quality].to_i
     if new_quality.between?(0, 2)
+      quality_params[:quality] = new_quality
       self.update_attributes(quality_params)
     else
       false
