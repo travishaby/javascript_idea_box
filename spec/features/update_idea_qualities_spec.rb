0d6d411df6af8ca6afd84ff37419bd43fab02cd6
Travis Haby <travishaby@gmail.com>
2015-12-02 18:27:50 -0700

    added extra test case to hit sad paths for increasing idea quality beyond scope

--- spec/features/update_idea_qualities_spec.rb
+++ spec/features/update_idea_qualities_spec.rb
@@ -9,10 +9,23 @@ RSpec.feature "UpdateIdeaQualities", type: :feature, js: true do
     expect(idea.quality).to eq("swill")
     expect(page).to have_content("swill")
 
-    find(:css, ".up-button").click
+
     find(:css, ".up-button").click
     idea = Idea.find_by(title: "title one")
-    expect(idea.quality).to eq("genius")
-    expect(page).to have_content("genius")
+    expect(idea.quality).to eq("plausible")
+    expect(page).to have_content("plausible")
+  end
+
+  scenario "can't make idea worse than swill" do
+    idea1
+    visit root_path
+
+    idea = Idea.find_by(title: "title one")
+    expect(idea.quality).to eq("swill")
+    expect(page).to have_content("swill")
+
+
+    find(:css, ".down-button").click
+    expect(page).to have_content("swill")
   end
 end
